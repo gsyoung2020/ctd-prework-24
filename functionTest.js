eec76dcd43034992dd6ef8e54f40e71a33b5a300
gsyoung2020 <gsyoung2019@gmail.com>
2024-03-21 16:50:54 -0400

    Starting working on the frontend UI of my site

--- functionTest.js
+++ functionTest.js
@@ -26,4 +26,32 @@ $(document).ready(function() {
     }
 
     startTime();
+//come back to this later
+    $(".slider").click(function() {
+        if ($(this).is(":checked")) {
+          // Toggle button is checked, convert to Celsius
+          $("#far").css("color", "white");
+          $("#cel").css("color", "yellow");
+          $(".minTemp, .maxTemp, .currntTemp, .aprtTemp").each(function() {
+            var tempValue = $(this).find("span:first-child").text();
+            if (!isNaN(tempValue)) {
+              var celsiusValue = ((parseFloat(tempValue) - 32) * 5) / 9;
+              $(this).find("span:first-child").text(celsiusValue.toFixed(1));
+            }
+          });
+          $(".tempEmblm").text("C");
+        } else {
+          // Toggle button is unchecked, convert to Fahrenheit
+          $("#far").css("color", "yellow");
+          $("#cel").css("color", "white");
+          $(".minTemp, .maxTemp, .currntTemp, .aprtTemp").each(function() {
+            var tempValue = $(this).find("span:first-child").text();
+            if (!isNaN(tempValue)) {
+              var fahrenheitValue = (parseFloat(tempValue) * 9) / 5 + 32;
+              $(this).find("span:first-child").text(fahrenheitValue.toFixed(1));
+            }
+          });
+          $(".tempEmblm").text("F");
+        }
+      });
 });
