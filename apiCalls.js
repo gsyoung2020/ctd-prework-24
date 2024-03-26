0c07fd6c4b85098d7e2bf07487697af75770328a
gsyoung2020 <gsyoung2019@gmail.com>
2024-03-25 21:47:03 -0400

    Work on 7 day functionality, have it so you can click on day and it shows you the day, Fixes: the current day dissappears, need to set up all the conversions

--- apiCalls.js
+++ apiCalls.js
@@ -57,6 +57,48 @@ let hourlytime = {
   24: "12 AM"
 }
 
+let daysFull = {
+  MON: "Monday",
+  TUE: "Tuesday",
+  WED: "Wednesday",
+  THU: "Thursday",
+  FRI: "Friday",
+  SAT: "Saturday",
+  SUN: "Sunday"
+} 
+
+function whatisToday(value) {
+  for (const [key, val] of Object.entries(daysFull)) {
+    if (key == value) {
+      return val;
+    }
+  }
+
+}
+
+function findMulti(arr) {
+  // Create an object to store the frequency count
+  const frequencyCount = {};
+
+  // Iterate through the array and update the frequency count
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    frequencyCount[num] = (frequencyCount[num] || 0) + 1;
+  }
+
+  // Find the number with the highest frequency count
+  let maxFrequency = 0;
+  let mode = null;
+  for (const num in frequencyCount) {
+    if (frequencyCount[num] > maxFrequency) {
+      maxFrequency = frequencyCount[num];
+      mode = num;
+    }
+  }
+
+  return mode;
+}
+
 function updateSlider(unit) {
   if (unit === "fahrenheit") {
     $(".switch input[type='checkbox']").prop("checked", true);
@@ -90,6 +132,36 @@ function hourlyIMG(weatherCode, index) {
     }
   }
 }
+}
+
+function currentIMG(weatherCode) {
+          // using the weather code to display the weather status and icon for the current weather
+          for (const [key, value] of Object.entries(weather_code)) {
+            if (key == weatherCode) {
+              $(".weatherStatus").html(value);
+                if(key == 0){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/sun.svg" alt="clear-sky">`);
+                } else if (key == 1 || key == 2 || key == 3){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/cloudy.svg" alt="cloudy">`);
+                } else if (key == 45 || key == 48){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/fog.svg" alt="Fog">`);
+                } else if (key == 51 || key == 53 || key == 55 || key == 56 || key == 57){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/drizzle.svg" alt="drizzle">`);
+                }
+                else if (key == 61 || key == 63 || key == 65 || key == 66 || key == 67){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/rain.svg" alt="rain">`);
+                }
+                else if (key == 71 || key == 73 || key == 75 || key == 77){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/snowy.svg" alt="snowy">`);
+                }
+                else if (key == 80 || key == 81 || key == 82 || key == 85 || key == 86){
+                  $("#wIcon").html(`<img class="weatherICON" src="assets/rainshowers.svg" alt="rain showers">`);
+                }
+                else if (key == 95 || key == 96 || key == 99){
+                  $("#wIcon").html(`<img  class="weatherICON" src="assets/thunderstorm.svg" alt="thunderstorm">`);
+            }
+          }
+        }
 }
 
 // Conversion functions
@@ -147,14 +219,16 @@ var dailyTemperatureData = [];
 var dailyPrecipitationData = [];
 var dailyWeatherCodeData = [];
 var dailyWindSpeedData = [];
+var dailyHumidityData = [];
 
 //Function to convert hourly temperature data into daily temperature data
-function processHourlyData(hourlyTemperature, hourlyPrecipitation, hourlyWeatherCode, hourlyWindSpeed) {
+function processHourlyData(hourlyTemperature, hourlyPrecipitation, hourlyWeatherCode, hourlyWindSpeed, hourlyHumidity) {
   const HOURS_IN_DAY = 24;
   dailyTemperatureData = []; // Reset or initialize the storage
   dailyPrecipitationData = [];
   dailyWeatherCodeData = [];
   dailyWindSpeedData = [];
+  dailyHumidityData = [];
   
   
   // Iterate over the hourlyData array in 24-hour chunks
@@ -164,10 +238,12 @@ function processHourlyData(hourlyTemperature, hourlyPrecipitation, hourlyWeather
       let dailyPrecip = hourlyPrecipitation.slice(i * HOURS_IN_DAY, (i + 1) * HOURS_IN_DAY);
       let dailyWeatherCode = hourlyWeatherCode.slice(i * HOURS_IN_DAY, (i + 1) * HOURS_IN_DAY);
       let dailyWindSpeed = hourlyWindSpeed.slice(i * HOURS_IN_DAY, (i + 1) * HOURS_IN_DAY);
+      let dailyHumidity = hourlyHumidity.slice(i * HOURS_IN_DAY, (i + 1) * HOURS_IN_DAY);
       dailyTemperatureData.push(dailyTemp);
       dailyPrecipitationData.push(dailyPrecip);
       dailyWeatherCodeData.push(dailyWeatherCode);
       dailyWindSpeedData.push(dailyWindSpeed);
+      dailyHumidityData.push(dailyHumidity);
   }
 }
 
@@ -211,8 +287,58 @@ function getTimezoneForCoordinates(latitude, longitude) {
   });
 }
 
+var currntTempStored = ""
+var storedPrecip = ""
+var storedWind = ""
+var storedHumid = ""
+
+//All this is broken and needs to be fixed
 function updateHourlyData(hourlyTM) {
-  alert("made it here");
+  var isInitialLoad = $(".day1").hasClass("stored");
+
+  if (isInitialLoad || hourlyTM === 0) {
+    currntTempStored = $("#currntValue").text();
+    storedPrecip = $("#precipValue").text();
+    storedWind = $("#windValue").text();
+    storedHumid = $("#humValue").text();
+    $(".day1").removeClass("stored");
+  }
+
+  var windSum = 0;
+  var precipSum = 0;
+  var tempSum = 0;
+  var humidSum = 0;
+  var selectedHigh = $('.sevenDayContent').children().eq(hourlyTM).find(`#dailyMax${hourlyTM+1}`).text();
+  var selectedLow = $('.sevenDayContent').children().eq(hourlyTM).find(`#dailyMin${hourlyTM+1}`).text();
+  $("#maxValue").html(selectedHigh);
+  $("#minValue").html(selectedLow);
+  var mostFrequentWeatherCode = findMulti(dailyWeatherCodeData[hourlyTM]);
+  currentIMG(mostFrequentWeatherCode);
+
+
+
+  if (hourlyTM === 0) {
+    $("#windValue").html(storedWind);
+    $("#precipValue").html(storedPrecip);
+    $("#currntValue").html(currntTempStored);
+    $("#humValue").html(storedHumid);
+  } else {
+    for (let i = 0; i < 23; i++) {
+      windSum += dailyTemperatureData[hourlyTM][i];
+      precipSum += dailyPrecipitationData[hourlyTM][i];
+      tempSum += dailyTemperatureData[hourlyTM][i];
+      humidSum += dailyHumidityData[hourlyTM][i];
+    }
+    var windAvg = Math.round(windSum / 24);
+    var precipAvg = Math.round(precipSum / 24);
+    var tempAvg = Math.round(tempSum / 24);
+    var humidAvg = Math.round(humidSum / 24);
+    $("#windValue").html(windAvg);
+    $("#precipValue").html(precipAvg);
+    $("#currntValue").html(tempAvg);
+    $("#humValue").html(humidAvg);
+  }
+
   for (let i = 0; i <= 23; i++) {
     if (i <= 11) {
       $(`#hour${i+1}`).html(hourlytime[i]);
@@ -240,7 +366,8 @@ function getWeatherData(latitude, longitude, callback) {
       openMeteo.hourly.temperature_2m,
       openMeteo.hourly.precipitation_probability,
       openMeteo.hourly.weather_code,
-      openMeteo.hourly.wind_speed_10m
+      openMeteo.hourly.wind_speed_10m,
+      openMeteo.hourly.relative_humidity_2m
     );
    /* dailyTemperatureData[0];
     dailyPrecipitationData[0];
@@ -312,33 +439,7 @@ function getWeatherData(latitude, longitude, callback) {
         var windSpeed = openMeteo.hourly.wind_speed_10m[i];
         var humidity = openMeteo.hourly.relative_humidity_2m[i];
         
-        // using the weather code to display the weather status and icon for the current weather
-        for (const [key, value] of Object.entries(weather_code)) {
-          if (key == weatherCode) {
-            $(".weatherStatus").html(value);
-              if(key == 0){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/sun.svg" alt="clear-sky">`);
-              } else if (key == 1 || key == 2 || key == 3){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/cloudy.svg" alt="cloudy">`);
-              } else if (key == 45 || key == 48){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/fog.svg" alt="Fog">`);
-              } else if (key == 51 || key == 53 || key == 55 || key == 56 || key == 57){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/drizzle.svg" alt="drizzle">`);
-              }
-              else if (key == 61 || key == 63 || key == 65 || key == 66 || key == 67){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/rain.svg" alt="rain">`);
-              }
-              else if (key == 71 || key == 73 || key == 75 || key == 77){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/snowy.svg" alt="snowy">`);
-              }
-              else if (key == 80 || key == 81 || key == 82 || key == 85 || key == 86){
-                $("#wIcon").html(`<img class="weatherICON" src="assets/rainshowers.svg" alt="rain showers">`);
-              }
-              else if (key == 95 || key == 96 || key == 99){
-                $("#wIcon").html(`<img  class="weatherICON" src="assets/thunderstorm.svg" alt="thunderstorm">`);
-          }
-        }
-      }
+        currentIMG(weatherCode);
       // For Seven Day Forecast that displays the weather icon
       for (let i = 0; i < 7; i++) {
         const weatherCode = openMeteo.daily.weather_code[i];
@@ -418,6 +519,9 @@ function getWeatherData(latitude, longitude, callback) {
       callback();
     }
   });
+  for (let i = 0; i < 7; i++) {
+    $(`.day${i+1}`).removeClass("dayActive");
+  }
   $(".day1").addClass("dayActive");
 }
 
@@ -429,16 +533,21 @@ $(document).ready(()=>{
 
   $(".sevenDayContent").children().click(function() {
     var clickedIndex = $(this).index();
-    alert("Clicked element index: " + clickedIndex);
+    var clickedDay = $(this).find(`#day${clickedIndex+1}`).text();
+    //alert("Clicked element index: " + clickedIndex);
+    //console.log($(this).find(`#day${clickedIndex+1}`).text());
     for (let i = 0; i < 7; i++) {
       if (clickedIndex == i) {
         $(`.day${i+1}`).addClass("dayActive");
+        if (clickedIndex === 0) {
+          $(`.day${i+1}`).addClass("stored");
+        }
       } else {
         $(`.day${i+1}`).removeClass("dayActive");
       }
     }
     updateHourlyData(clickedIndex);
-
+    $(".today").html(whatisToday(clickedDay.toUpperCase()));
   })
 
   //This is for the hourly weather data
